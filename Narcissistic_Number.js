5b512e21aca804e3ed909198b590eb24a443c3d6
João Gabriel <143552261+Jotagamaral@users.noreply.github.com>
2023-09-06 17:33:38 -0300

    Update Narcissistic_Number.js

--- Narcissistic_Number.js
+++ Narcissistic_Number.js
@@ -1,4 +1,9 @@
-A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+//A Narcissistic Number (or Armstrong Number) is a positive 
+//number which is the sum of its own digits, each raised to 
+//the power of the number of digits in a given base. Your code
+//must return true or false (not 'true' and 'false') depending
+//upon whether the given number is a Narcissistic number in base 10.
+
 function narcissistic(value) {
 
     let number = 0
