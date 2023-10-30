1409ee7adcdda666623894cff2c06131fd9da998
João Gabriel <143552261+Jotagamaral@users.noreply.github.com>
2023-10-30 15:43:35 -0300

    Create Maximum_number_of_vowels_in_a_substring_of_given_length.js
    Return the maximum number of vowel letters in any substring of s with length k

---
+++ Maximum_number_of_vowels_in_a_substring_of_given_length.js
@@ -0,0 +1,24 @@
+// Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+// Input: s = "abciiidef", k = 3
+// Output: 3
+
+// Input: s = "aeiou", k = 2
+// Output: 2
+
+// Input: s = "leetcode", k = 3
+// Output: 2
+
+var maxVowels = function(s, k) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let result = 0, l = 0, count = 0
+    for (let i = 0; i < s.length; i++) {
+        count = vowels.includes(s[i]) ? count+=1 : count
+        if ((i - l + 1) > k) {
+            count = vowels.includes(s[l]) ? count-=1 : count
+            l++
+        }
+        result = Math.max(count, result)
+    }
+    return result
+};
